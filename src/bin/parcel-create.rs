16d51fd7c319796e5c38d7b284b9c7258e92302f
chordtoll <git@chordtoll.com>
2021-04-07 23:06:36 -0700

    Add attr support to parcel-create

--- src/bin/parcel-create.rs
+++ src/bin/parcel-create.rs
@@ -3,40 +3,69 @@ extern crate walkdir;
 
 extern crate parcel;
 
-use std::io::Cursor;
-
+use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::path::{Path,PathBuf};
 use clap::{Arg, App};
 use walkdir::WalkDir;
-use parcel::Parcel;
+use parcel::{Parcel,InodeAttr};
 
 fn main() {
-    let matches = App::new("My Super Program")
-                          .version("1.0")
-                          .author("Kevin K. <kbknapp@gmail.com>")
-                          .about("Does awesome things")
-                          .arg(Arg::with_name("output")
-                               .value_name("OUTPUT")
-                               .help("The output parcel to generate")
-                               .takes_value(true))
-                          .arg(Arg::with_name("input")
-                               .value_name("INPUT")
-                               .help("The input directory to scan")
-                               .multiple(true)
-                               .takes_value(true))
-                          .get_matches();
+    let matches = App::new("Parcel-Create")
+                            .version("0.1.0")
+                            .author("chordtoll <git@chordtoll.com>")
+                            .about("Creates parcel archive files for the pyxis package manager")
+                            .arg(Arg::with_name("output")
+                                .value_name("OUTPUT")
+                                .help("The output parcel to generate")
+                                .takes_value(true))
+                            .arg(Arg::with_name("input")
+                                .value_name("INPUT")
+                                .help("The input directory to scan")
+                                .multiple(true)
+                                .takes_value(true))
+                            .get_matches();
 
     let mut parcel : Parcel = Parcel::new();
 
-    let ino1 = parcel.add_file(parcel::FileAdd::Bytes(b"foo".to_vec()));
-    let ino2 = parcel.add_file(parcel::FileAdd::Bytes(b"bar".to_vec()));
-    let ino3 = parcel.add_file(parcel::FileAdd::Name("Cargo.toml".to_string()));
+    let mut dir_map: HashMap<PathBuf,u64> = HashMap::new();
+    dir_map.insert(PathBuf::from("/"),1);
 
-    parcel.insert_dirent(1,"foo.txt".to_string()   ,ino1);
-    parcel.insert_dirent(1,"bar.txt".to_string()   ,ino2);
-    parcel.insert_dirent(1,"Cargo.toml".to_string(),ino3);
+    for input in matches.value_of("input") {
+        for entry in WalkDir::new(input).min_depth(1) {
+            let entry = entry.unwrap();
+            let entry_path = Path::new("/").join(entry.path().strip_prefix(input).unwrap());
+            let parent_inode = dir_map.get(entry_path.parent().unwrap()).unwrap().clone();
+            let entry_name = entry_path.file_name().unwrap();
 
-    let mut buf : Vec<u8> = Vec::new();
-    parcel.store(Cursor::new(&mut buf));
-    println!("{}",String::from_utf8_lossy(&buf));
+            let meta = entry.metadata().unwrap();
+            let file_type = meta.file_type();
 
+            if file_type.is_file() {
+                let attrs = InodeAttr::from_meta(&meta);
+                let ino = parcel.add_file(parcel::FileAdd::Name(entry.path().as_os_str().to_os_string()),attrs);
+                dir_map.insert(entry_path.clone(),ino);
+                parcel.insert_dirent(parent_inode,entry_name.to_os_string(),ino);
+            }
+            else if file_type.is_dir() {
+                let attrs = InodeAttr::from_meta(&meta);
+                let ino = parcel.add_directory(attrs);
+                dir_map.insert(entry_path.clone(),ino);
+                parcel.insert_dirent(parent_inode,entry_name.to_os_string(),ino);
+            }
+            else if file_type.is_symlink() {
+                let attrs = InodeAttr::from_meta(&meta);
+                let ino = parcel.add_symlink(fs::read_link(entry.path()).unwrap().as_os_str().to_os_string(),attrs);
+                dir_map.insert(entry_path.clone(),ino);
+                parcel.insert_dirent(parent_inode,entry_name.to_os_string(),ino);
+            }
+            else {
+                panic!("Unknown file type: {:?}",file_type);
+            }
+        }
+    }
+
+    let outfile = File::create(matches.value_of("output").unwrap()).unwrap();
+    parcel.store(outfile);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,10 @@ use std::io::prelude::*;
 use std::io::SeekFrom;
 use std::fs;
 use std::fs::File;
+use std::os::linux::fs::MetadataExt;
+use std::ffi::OsString;
 use std::collections::HashMap;
+use std::time::{SystemTime,UNIX_EPOCH,Duration};
 
 use serde::{Serialize, Deserialize};
 
@@ -19,12 +22,52 @@ const PARCEL_VERSION: u32 = 0;
 enum InodeKind {
     Directory,
     RegularFile,
+    Symlink,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InodeAttr {
+    pub atime: SystemTime,
+    pub mtime: SystemTime,
+    pub ctime: SystemTime,
+    pub perm:  u32,
+    pub nlink: u32,
+    pub uid:   u32,
+    pub gid:   u32,
+    pub rdev:  u64,
+}
+
+impl InodeAttr {
+    pub fn from_meta(meta: &dyn MetadataExt) -> InodeAttr {
+        InodeAttr {
+            atime: UNIX_EPOCH+Duration::new(meta.st_atime() as u64,meta.st_atime_nsec() as u32),
+            ctime: UNIX_EPOCH+Duration::new(meta.st_atime() as u64,meta.st_atime_nsec() as u32),
+            mtime: UNIX_EPOCH+Duration::new(meta.st_atime() as u64,meta.st_atime_nsec() as u32),
+            uid:   meta.st_uid(),
+            gid:   meta.st_gid(),
+            nlink: 1,
+            perm:  meta.st_mode(),
+            rdev:  meta.st_rdev(),
+        }
+    }
+}
+
+const ROOT_ATTRS: InodeAttr = InodeAttr {
+    atime: UNIX_EPOCH,
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    perm: 0o755,
+    nlink: 1,
+    uid: 0,
+    gid: 0,
+    rdev: 0,
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 enum InodeContent {
     Directory(HashMap<String,u64>),
     RegularFile(FileReference),
+    Symlink(String),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -37,6 +80,7 @@ struct FileReference {
 struct Inode {
     kind: InodeKind,
     parent: u64,
+    attrs: InodeAttr,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -58,7 +102,7 @@ pub struct Parcel {
 #[derive(Debug)]
 pub enum FileAdd {
     Bytes(Vec<u8>),
-    Name(String),
+    Name(OsString),
 }
 
 impl Parcel {
@@ -74,7 +118,7 @@ impl Parcel {
             to_add: HashMap::new(),
         };
         
-        parcel.inodes.insert(1,Inode { kind: InodeKind::Directory, parent: 0 });
+        parcel.inodes.insert(1,Inode { kind: InodeKind::Directory, parent: 0, attrs: ROOT_ATTRS });
 
         parcel.content.insert(1,InodeContent::Directory(HashMap::new()));
 
@@ -130,12 +174,12 @@ impl Parcel {
         }
     }
 
-    pub fn add_file(&mut self, from: FileAdd) -> u64 {
+    pub fn add_file(&mut self, from: FileAdd, attrs: InodeAttr) -> u64 {
         while self.inodes.contains_key(&self.next_inode) {
             self.next_inode += 1;
         }
 
-        self.inodes.insert(self.next_inode, Inode {kind: InodeKind::RegularFile, parent: 0});
+        self.inodes.insert(self.next_inode, Inode {kind: InodeKind::RegularFile, parent: 0, attrs: attrs});
 
         let filesize = match &from {
             FileAdd::Bytes(i) => i.len() as u64,
@@ -150,9 +194,33 @@ impl Parcel {
         self.next_inode-1
     }
 
-    pub fn insert_dirent(&mut self, parent: u64, name: String, child: u64) {
+    pub fn add_directory(&mut self, attrs: InodeAttr) -> u64 {
+        while self.inodes.contains_key(&self.next_inode) {
+            self.next_inode += 1;
+        }
+
+        self.inodes.insert( self.next_inode, Inode {kind: InodeKind::Directory, parent: 0, attrs: attrs});
+        self.content.insert(self.next_inode,InodeContent::Directory(HashMap::new()));
+
+        self.next_inode+=1;
+        self.next_inode-1
+    }
+
+    pub fn add_symlink(&mut self, target: OsString, attrs: InodeAttr) -> u64 {
+        while self.inodes.contains_key(&self.next_inode) {
+            self.next_inode += 1;
+        }
+
+        self.inodes.insert( self.next_inode, Inode {kind: InodeKind::Symlink, parent: 0, attrs: attrs});
+        self.content.insert(self.next_inode,InodeContent::Symlink(target.into_string().unwrap()));
+
+        self.next_inode+=1;
+        self.next_inode-1
+    }
+
+    pub fn insert_dirent(&mut self, parent: u64, name: OsString, child: u64) {
         match self.content.get_mut(&parent).unwrap() {
-            InodeContent::Directory(dir) => dir.insert(name,child),
+            InodeContent::Directory(dir) => dir.insert(name.into_string().unwrap(),child),
             _ => panic!(),
         };
         self.inodes.get_mut(&child).unwrap().parent = parent;
@@ -190,15 +258,75 @@ mod tests {
               1:
                 kind: Directory
                 parent: 0
+                attrs:
+                  atime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  mtime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  ctime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  perm: 493
+                  nlink: 1
+                  uid: 0
+                  gid: 0
+                  rdev: 0
               2:
                 kind: RegularFile
                 parent: 1
+                attrs:
+                  atime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  mtime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  ctime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  perm: 0
+                  nlink: 1
+                  uid: 0
+                  gid: 0
+                  rdev: 0
               4:
                 kind: RegularFile
                 parent: 1
+                attrs:
+                  atime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  mtime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  ctime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  perm: 0
+                  nlink: 1
+                  uid: 0
+                  gid: 0
+                  rdev: 0
               3:
                 kind: RegularFile
                 parent: 1
+                attrs:
+                  atime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  mtime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  ctime:
+                    secs_since_epoch: 0
+                    nanos_since_epoch: 0
+                  perm: 0
+                  nlink: 1
+                  uid: 0
+                  gid: 0
+                  rdev: 0
             content:
               1:
                 Directory:
