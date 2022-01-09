c2489532ffe074a1826fd7adcf7474103880a65c
chordtoll <git@chordtoll.com>
2022-01-08 20:11:49 -0800

    Update reoundtrip test to use convenience method

--- tests/many_files.rs
+++ tests/many_files.rs
@@ -1,6 +1,4 @@
-use std::{fs::File, path::PathBuf};
-
-use pyxis_parcel::{FileAdd, ParcelHandle, ReaderWriter};
+use pyxis_parcel::{FileAdd, ParcelHandle};
 use rand::{distributions::Alphanumeric, Rng, SeedableRng};
 use rand_pcg::Pcg64;
 
@@ -11,9 +9,7 @@ use common::Fixture;
 fn many_file_roundtrip() {
     let f = Fixture::blank("test.parcel");
     let mut parcel = ParcelHandle::new();
-    parcel.set_file(Box::new(ReaderWriter::new(
-        File::create(PathBuf::from(&f)).unwrap(),
-    )));
+    parcel.set_file(f.make_rw());
 
     let mut rng = Pcg64::seed_from_u64(0);
     for i in 0..100 {
@@ -34,10 +30,7 @@ fn many_file_roundtrip() {
 
     parcel.store().unwrap();
 
-    let mut parcel = ParcelHandle::load(Box::new(ReaderWriter::new(
-        File::open(PathBuf::from(&f)).unwrap(),
-    )))
-    .unwrap();
+    let mut parcel = ParcelHandle::load(f.make_rw()).unwrap();
     let mut rng = Pcg64::seed_from_u64(0);
     for i in 0..100 {
         let length = rng.gen_range(0..100);
